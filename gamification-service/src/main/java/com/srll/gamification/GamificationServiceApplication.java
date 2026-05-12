2411d4c87fbcf99e710fa89ce2ad0637a3a0b55b
Tolga Boz <tolgaboz350@gmail.com>
2026-05-12 18:38:58 +0300

    Add initial service applications and configuration files for api-gateway, auth-service, card-service, and gamification-service

---
+++ gamification-service/src/main/java/com/srll/gamification/GamificationServiceApplication.java
@@ -0,0 +1,11 @@
+package com.srll.gamification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamificationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GamificationServiceApplication.class, args);
+    }
+}
